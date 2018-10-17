25fcee0ca9d327fe36a4f2b77e422136cc60e0ae
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-10-17 11:46:38 +0200

    Create project shell

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 /*
  -----------------------------------------------------------------------------------
- Laboratoire : <nn>
+ Laboratoire : 03
  Fichier     : main.cpp
- Auteur(s)   : Doran Kayoumi
+ Auteur(s)   : Rosalie Chhen, Robin Reuteler, Doran Kayoumi
  Date        : 17.10.2018
 
- But         : <à compléter>
+ But         : Mettre en place un système de comptage de point pour le jeu de 
+               fléchette 501 double out.
 
- Remarque(s) : <à compléter>
+ Remarque(s) : Les valeurs données par l'utilisateur ne sont pas fiable.
 
  Compilateur : MinGW-g++ <6.3.0>
  -----------------------------------------------------------------------------------
@@ -18,11 +19,27 @@
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main() {
+   
+   const int THROWS_PER_SET = 3;
+   const int STARTING_SCORE = 501;
+   
+   int totalScore = STARTING_SCORE;
+   int currentScore = STARTING_SCORE;
+   int nbThrows = 0;
+   
+   while (currentScore != 0) {
+      
+      // get user input
+      do {
+         
+      }
+      while (true);
+      
+      
+      
+   }
+   
    return 0;
 }
 
