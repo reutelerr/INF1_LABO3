702b71cccd99bceea459b937a2531dfa64ab64d1
Doran Kayoumi <dorankayoumi@gmail.com>
2018-10-17 11:31:27 +0200

    Initial commit 2.0

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : <nn>
+ Fichier     : main.cpp
+ Auteur(s)   : Doran Kayoumi
+ Date        : 17.10.2018
+
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : MinGW-g++ <6.3.0>
+ -----------------------------------------------------------------------------------
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   return 0;
+}
+
