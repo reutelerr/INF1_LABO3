853dcaf88b7c674b3d35129bbc3e08e5aec294fb
reutelerr <44259320+reutelerr@users.noreply.github.com>
2018-10-18 11:28:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@
  Fichier     : main.cpp
  Auteur(s)   : Rosalie Chhen, Robin Reuteler, Doran Kayoumi
  Date        : 17.10.2018
-
- But         : Mettre en place un système de comptage de point pour le jeu de 
+ But         : Mettre en place un système de comptage de point pour le jeu de
                fléchette 501 double out.
-
  Remarque(s) : Les valeurs données par l'utilisateur ne sont pas fiable.
-
  Compilateur : MinGW-g++ <6.3.0>
  -----------------------------------------------------------------------------------
  */
@@ -20,26 +17,55 @@
 using namespace std;
 
 int main() {
-   
+
    const int THROWS_PER_SET = 3;
    const int STARTING_SCORE = 501;
-   
+
    int totalScore = STARTING_SCORE;
    int currentScore = STARTING_SCORE;
-   int nbThrows = 0;
-   
+   int nbThrow = 0;
+
    while (currentScore != 0) {
-      
+
       // get user input
+      string throwValue;
+      bool validInput(0);
+      bool validNum(0);
       do {
-         
+            cout<<"Score: "<<currentScore<<"  - Jouez la flechette "<<nbThrow<<"/"<<THROWS_PER_SET<<endl;
+            if(not(cin>>throwValue) or throwValue.empty())
+            {
+                throwValue="_";
+            }
+
+            switch(throwValue[0])
+            {
+                case 'T':
+                {
+                    validInput=true;
+                    break;
+                }
+
+                case 'D':
+                {
+                    validInput=true;
+                    break;
+                }
+
+                default:
+                {
+                    validInput=false;
+                    cout<<"Entree non valide"<<endl;
+                }
+            }
       }
-      while (true);
-      
-      
-      
+      while (validInput and validNum);
+
+
+
    }
-   
+
    return 0;
 }
+
 
