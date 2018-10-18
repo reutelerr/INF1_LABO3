4a2d4b5d1c28a2883370d4bc9918de244bd9f1f2
Doran Kayoumi <dorankayoumi@gmail.com>
2018-10-18 08:44:47 +0200

    Retrieve user input and manage score value

--- main.cpp
+++ main.cpp
@@ -16,30 +16,59 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <typeinfo>
 
 using namespace std;
 
 int main() {
-   
+
    const int THROWS_PER_SET = 3;
    const int STARTING_SCORE = 501;
+   const int MIN_SCORE = 0;
+   const int MAX_SCORE = 20;
+   const int BULLSEYE = 25;
    
    int totalScore = STARTING_SCORE;
    int currentScore = STARTING_SCORE;
    int nbThrows = 0;
+   int score;
    
+   string inputScore;
+   // flag to determine if the user inputed a correct value
+   bool error;
+   
+   do {
+      error = false;
+      
+      cout << "Score: ## - Jouez la flechette $$/3" << endl;
+      cin  >> inputScore;
+     
+      // check if a value was entered
+      if (inputScore.empty()) error = true;
+      
+      
+      stringstream ss(inputScore);
+      
+      ss >> score;
+      
+      if ((score < MIN_SCORE or score > MAX_SCORE) and score != BULLSEYE) 
+         error = true;
+      
+   } while (error and 
+           cout << "Entree non valide" << endl);
+   
+   /*
    while (currentScore != 0) {
       
       // get user input
       do {
          
       }
-      while (true);
-      
-      
-      
+      while (true); 
    }
-   
+   */
    return 0;
 }
 
